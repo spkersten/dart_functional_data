35d8782b9b4679f80492366bbdd3da9f013c4c7c
Sander Kersten <spkersten@gmail.com>
2018-12-16 19:32:24 +0100

    Fix example

--- example/README.md
+++ example/README.md
@@ -1,4 +1,7 @@
 # Example of functional_data
 
+Generate code:
+`pub run build_runner build lib`
+
 Run:
 `dart bin/main.dart`
--- example/bin/main.dart
+++ example/bin/main.dart
@@ -39,8 +39,8 @@ main(List<String> arguments) {
   print(firstFooName.of(bar).update("Twee"));
   // Bar(foo: Foo(number: 42, name: Marvin), foos: [Foo(number: 101, name: Twee), Foo(number: 102, name: Two)], driver: One)
 
-  final fooNamedTwo = Bar$.foos.then(List$.where<Foo>((foo) => foo.name == "Two")).then(Foo$.name);
-  print(fooNamedTwo.update(bar, "Due"));
+  final nameOfFooNamedTwo = Bar$.foos.then(List$.where<Foo>((foo) => foo.name == "Two")).then(Foo$.name);
+  print(nameOfFooNamedTwo.update(bar, "Due"));
   // Bar(foo: Foo(number: 42, name: Marvin), foos: [Foo(number: 101, name: One), Foo(number: 102, name: Due)], driver: One)
 
   final driversNumber =
