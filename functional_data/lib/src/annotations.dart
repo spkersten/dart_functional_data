9056a7c483be6a11b18d996fd779347928b51d58
Gabriel Gava <gabrielgarciagava@gmail.com>
2023-06-01 14:01:34 +0200

    Added arguments to configure which parts of the code should be generated
    Fields added:
    
    generateCopy
    generateCopyWith
    generateCopyUsing
    generateLenses
    
    Documentation can be found in annotations.dart

--- functional_data/lib/src/annotations.dart
+++ functional_data/lib/src/annotations.dart
@@ -1,7 +1,37 @@
 import 'package:collection/collection.dart';
 
 class FunctionalData {
-  const FunctionalData();
+  const FunctionalData({
+    this.generateCopy,
+    this.generateCopyWith,
+    this.generateCopyUsing,
+    this.generateLenses,
+  });
+
+  /// Whether the generator should generate the copy methods.
+  ///
+  /// When set, it overrides the settings of [generateCopyWith] and [generateCopyUsing].
+  final bool? generateCopy;
+
+  /// Whether the generator should generate the copyWith method.
+  ///
+  /// Defaults to `true`.
+  final bool? generateCopyWith;
+
+  /// Whether the generator should generate the copyUsing method.
+  ///
+  /// When set to `true`, a helper class will also be generated (called `ClassName$Change`).
+  ///
+  /// Defaults to `true`.
+  final bool? generateCopyUsing;
+
+  /// Whether the generator should generate the lenses class.
+  ///
+  /// The lenses class uses the `copyWith` method. So if [generateLenses] is set to `true`,
+  /// it will ignore [generateCopyWith] configuration and force generate the copyWith method.
+  ///
+  /// Defaults to `false`.
+  final bool? generateLenses;
 }
 
 class CustomEquality {
